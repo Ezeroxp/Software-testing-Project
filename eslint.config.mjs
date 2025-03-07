b9759a1185ecf239cea290474526e2d67f652291
Xabier Pascal <pascal.148633@e.unavarra.es>
2025-03-07 17:50:43 +0100

    [#5] - added linter to ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ["dist/**"],
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}", "src/**/*.{js,ts}"],
+    languageOptions: {
+      parser: tsparser,
+      globals: {...globals.browser, process: true},
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "no-unused-vars": "off", // Explicitly disable the base rule
+      "@typescript-eslint/no-unused-vars": ["off", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+
+      "@typescript-eslint/no-unused-expressions": ["error", { allowShortCircuit: true }],
+    },
+  },
 ];
