c73019cf55a8ce2091d43053297411dfaeb2ff5a
Xabier Pascal <pascal.148633@e.unavarra.es>
2025-03-07 18:09:44 +0100

    [#5] - Added prettier for formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,41 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
-    ignores: ["dist/**"],
+    ignores: ['dist/**'],
   },
   pluginJs.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts}", "src/**/*.{js,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}', 'src/**/*.{js,ts}'],
     languageOptions: {
       parser: tsparser,
-      globals: {...globals.browser, process: true},
-      ecmaVersion: "latest",
-      sourceType: "module",
+      globals: { ...globals.browser, process: true },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
     plugins: {
-      "@typescript-eslint": tseslint,
+      '@typescript-eslint': tseslint,
+      prettier: eslintPluginPrettier,
     },
     rules: {
-      "no-unused-vars": "off", // Explicitly disable the base rule
-      "@typescript-eslint/no-unused-vars": ["off", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
-
-      "@typescript-eslint/no-unused-expressions": ["error", { allowShortCircuit: true }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'off',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true },
+      ],
+      'prettier/prettier': [
+        'error',
+        { singleQuote: true, semi: true, trailingComma: 'es5' },
+      ],
     },
   },
-];
+]
