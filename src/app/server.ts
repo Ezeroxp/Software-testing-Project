e0496de5730074049035c8718930163fa684e7b1
TheBloodMan49 <79766427+TheBloodMan49@users.noreply.github.com>
2025-03-08 16:14:44 +0100

    [#11] Create front-end base (#15)
    * [#11] Create front-end base
    
    * [#11] Front-end functional with test data
    
    * [#11] - Resolved conflicts
    
    ---------
    
    Co-authored-by: Xabier Pascal <pascal.148633@e.unavarra.es>

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,9 +1,22 @@
 import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import path from "path";
 
 const server = fastify();
 
+// Register the fastify-static plugin
+server.register(fastifyStatic, {
+    root: path.join(__dirname, "frontend"),
+    prefix: "/public/",
+});
+
 server.get("/ping", async (request, reply) => {
     return "pong\n";
+});
+
+server.get("/", async (request, reply) => {
+    // Return the content at the route /public/index.html
+    return reply.redirect("/public/index.html");
 });
 
 server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
