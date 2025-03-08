c57f24d5ff1ee56178a8959878e0d9f974864eb8
EnekoIsturitzSesma <152178641+EnekoIsturitzSesma@users.noreply.github.com>
2025-03-08 16:23:36 +0100

    12 get album data (#18)
    * [12]Api Calls
    
    * [12]Api Calls
    
    * [#12] - Refactored tests
    
    ---------
    
    Co-authored-by: Xabier Pascal <pascal.148633@e.unavarra.es>

---
+++ src/app/apiConnectivity/__tests__/getAlbumData.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it, beforeAll } from "vitest";
+import dotenv from "dotenv";
+import { getAlbumData } from "../getAlbumData";
+import { getAccessToken } from "../getToken";
+
+dotenv.config();
+
+describe("getAlbumData", () => {
+    const albumId = "4aawyAB9vmqN3uQ7FjRGTy";
+    let accessToken: string;
+    beforeAll(async () => {
+        const clientId = process.env.CLIENT_ID!;
+        const clientSecret = process.env.CLIENT_SECRET!;
+
+        accessToken = await getAccessToken(clientId, clientSecret);
+    })
+
+    it("obtains album data", async () => {
+        const albumData = await getAlbumData(accessToken, albumId);
+
+        expect(albumData).toBeDefined();
+        expect(albumData.name).toBeDefined();
+        expect(albumData.artists).toBeInstanceOf(Array);
+    });
+    
+    it("does not obtain album data of nonexistent album", async () => {
+        const fakeAlbumId = "1234567890abcdef";
+
+        expect(await getAlbumData(accessToken, fakeAlbumId)).toThrow(Error);
+    });
+
+    it("does not obtain album data if token is invalid", async () => {
+        expect(await getAlbumData("invalidToken", albumId)).toThrow(Error);
+    })
+});
---
+++ src/app/apiConnectivity/getAlbumData.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import dotenv from "dotenv";
+import { getAccessToken } from "./getToken"; 
+
+dotenv.config();
+
+const BASE_URL = "https://api.spotify.com/v1/albums";
+
+
+export const getAlbumData = async (token: string, albumId: string) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/${albumId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        return response.data;
+    } catch (error: any) {
+        console.error("Error obtaining album data:", error.response?.data || error.message);
+    }
+};
+
