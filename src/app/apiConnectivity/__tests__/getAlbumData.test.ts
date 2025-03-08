b0f37bb3c6cf80e9ff1889dd79ffb23b4049eb24
Xabier Pascal <pascal.148633@e.unavarra.es>
2025-03-08 16:21:46 +0100

    [#12] - Refactored tests

--- src/app/apiConnectivity/__tests__/getAlbumData.test.ts
+++ src/app/apiConnectivity/__tests__/getAlbumData.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, beforeAll } from "vitest";
 import dotenv from "dotenv";
 import { getAlbumData } from "../getAlbumData";
 import { getAccessToken } from "../getToken";
@@ -7,27 +7,29 @@ dotenv.config();
 
 describe("getAlbumData", () => {
     const albumId = "4aawyAB9vmqN3uQ7FjRGTy";
-
-    it("obtains album data", async () => {
+    let accessToken: string;
+    beforeAll(async () => {
         const clientId = process.env.CLIENT_ID!;
         const clientSecret = process.env.CLIENT_SECRET!;
 
-        const accessToken = await getAccessToken(clientId, clientSecret);
+        accessToken = await getAccessToken(clientId, clientSecret);
+    })
 
-        const albumData = await getAlbumData(albumId);
+    it("obtains album data", async () => {
+        const albumData = await getAlbumData(accessToken, albumId);
+
         expect(albumData).toBeDefined();
         expect(albumData.name).toBeDefined();
         expect(albumData.artists).toBeInstanceOf(Array);
     });
     
-    it("non-existing album id", async () => {
+    it("does not obtain album data of nonexistent album", async () => {
         const fakeAlbumId = "1234567890abcdef";
 
-        try {
-            await getAlbumData(fakeAlbumId);
-        } catch (error: any) {
-            expect(error).toBeDefined();
-            expect(error.response.status).toBe(404);
-        }
+        expect(await getAlbumData(accessToken, fakeAlbumId)).toThrow(Error);
     });
+
+    it("does not obtain album data if token is invalid", async () => {
+        expect(await getAlbumData("invalidToken", albumId)).toThrow(Error);
+    })
 });
--- src/app/apiConnectivity/getAlbumData.ts
+++ src/app/apiConnectivity/getAlbumData.ts
@@ -7,15 +7,11 @@ dotenv.config();
 const BASE_URL = "https://api.spotify.com/v1/albums";
 
 
-export const getAlbumData = async (albumId: string) => {
+export const getAlbumData = async (token: string, albumId: string) => {
     try {
-        const clientId = process.env.CLIENT_ID!;
-        const clientSecret = process.env.CLIENT_SECRET!;
-        const accessToken = await getAccessToken(clientId, clientSecret);
-
         const response = await axios.get(`${BASE_URL}/${albumId}`, {
             headers: {
-                Authorization: `Bearer ${accessToken}`,
+                Authorization: `Bearer ${token}`,
             },
         });
 
